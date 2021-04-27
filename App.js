6d1f242afa71b4e059afed60b4273155a20de969
forfeen <f.fin07@gmail.com>
2021-04-27 14:01:30 +0700

    💄 update components in App.js

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import IndexScreen from './src/screens/IndexScreen';
+import DetailScreen from './src/screens/DetailScreen';
+import {Provider} from './src/context/VaccineContext';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createStackNavigator({
+  Index: IndexScreen,
+  Detail: DetailScreen
+}, {
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Ceen'
+  }
 });
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <Provider>
+  <App />
+  </Provider>
+};
