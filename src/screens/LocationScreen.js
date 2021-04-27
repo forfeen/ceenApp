a6f2ddc966233fa5ef3d4fb61267dca43836937c
forfeen <f.fin07@gmail.com>
2021-04-27 23:50:03 +0700

    add location screen

---
+++ src/screens/LocationScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, StyleSheet, FlatList, TouchableOpacity, Image} from 'react-native';
+
+const LocationScreen = ({navigation}) => {
+    return(
+        <View> 
+            <Text> ddd </Text>  
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default LocationScreen;
